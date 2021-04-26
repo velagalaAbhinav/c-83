cd49d3fbc8128f47debfd83695686ff9c7f39448
velagalaAbhinav <62015508+velagalaAbhinav@users.noreply.github.com>
2021-04-26 14:13:12 +0530

    Add files via upload

---
+++ screens/NotificationScreen.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import{View,StyleSheet,Text,Image,TouchableOpacity,TextInput,Alert,Morda,KeyboardAvoidingView} from 'react-native'
+import db from '../config';
+import firebase from 'firebase'
+import MyHeader from '../Components/myheader'
+
+export default class notificationScreen extends React.Component{
+    render(){
+        return(
+            
+        )
+    }
+}
